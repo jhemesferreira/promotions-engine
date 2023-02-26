bbd048f97913e86c68371e43a8ff13b50c23f946
jhemesferreira <fs.jhemes@gmail.com>
2023-02-26 11:44:35 +0100

    dev: create lowestPrice controller with mock result json

---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductsController
+{
+    #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: Request::METHOD_POST)]
+    public function lowestPrice(Request $request, int $id)
+    {
+
+        if($request->headers->has('force_fail')){
+            return new JsonResponse(
+                ['error' => 'Promotions Engine failure message'],
+                $request->headers->get('force_fail')
+            );
+        }
+
+        return new JsonResponse([
+            "quantity" => 5,
+            "request_location" => "UK",
+            "voucher_code"  => "OU812",
+            "request_date" => "2022-04-04",
+            "product_id" => $id,
+            'price' => 100,
+            'discounted_price' => 50, 
+            'promotion_id' => 3,
+            'promotion_name' => 'Black Friday half price sale'
+        ], 200);
+
+    }
+}
